b08f9c652683bae33dc7124244e79674a34ec4cc
Chris Williams <chrisbw@gmail.com>
2012-02-22 18:40:18 -0500

    Cleaned up some HTML header info

--- venu.php
+++ venu.php
@@ -1,9 +1,6 @@
-<!DOCTYPE html
-	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" xml:lang="en-US">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-<!-- The above part can come from a script... -->
 <head>
 <title>Venu Music Database</title>
 </head>
@@ -17,7 +14,7 @@ div.block{
 
 div.block label{
   font-family: Verdana, Arial; 
-  width: 160px;
+  width: 120px;
   display: block;
   float: left;
   text-align: left;
