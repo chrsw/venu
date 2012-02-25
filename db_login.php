4039fb3a97fe2bf0449978ec3a3064d6dd6f7f57
Chris Williams <chrisbw@gmail.com>
2012-02-25 16:59:55 -0500

    DB login info in a seperate file. Limit the number of results the table displays.

--- db_login.php
+++ db_login.php
@@ -1,3 +1,10 @@
 <?php
 // placeholder
+// Use this for database connectivity test
+$test_database = "mysql";   // The database used for testing during development
+$database = "venu";         // The main music database
+$table = "releases";        // Name of table where actual release data is stored
+$host = "localhost";        // hostname of the mysql server
+$user = "admin";            // New MySQL user with privs for accessing the db
+$password = "password";     // DB access password, eventually there will be
 ?>
