77c666a0c8111f8f0f131f30667d2a0812ede277
Chris Williams <chrisbw@gmail.com>
2012-03-10 12:39:03 -0500

    Changed top banner. Fixed html tag issue. Updated release age funtion. Changed default page font.

--- venu.php
+++ venu.php
@@ -5,7 +5,7 @@ echo <<<_END
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-
+<html>
 <link rel="stylesheet" href="style.css" type="text/css" media="all"/>
 <head>
 
@@ -15,14 +15,15 @@ echo <<<_END
 
 <body>
 
-<div class="header">Venu Music Database <header class="right">A simple way to track music</header></div>
-<p>
+<div class="header">Venu Music Database</div>
 _END;
 
 // Read the database access info from a file in the form of
 // host:username:password:database:table
 $db_auth_file_name = "db_auth.txt";
-$auth_info = file_get_contents("$db_auth_file_name") or die("Could not get database authorization info.\n<br>");
+$auth_info = file_get_contents("$db_auth_file_name") or
+        die("Could not get database authorization info.\n<br>");
+
 // Store the password info across some string vars
 list($host, $username, $password, $database, $table) = split(":",$auth_info);
 
@@ -30,8 +31,9 @@ $table_length = 50;
 
 // Eastern timezone
 date_default_timezone_set(EST);
-echo "<br>Welcome to Venu, a web based system for tracking music. ";
-echo " It is ".date("l\, jS \of F\, Y") . ".<p>";
+
+// Body style banner message
+//echo "<br>Welcome to Venu, a web based system for tracking music. ";
 
 // TODO: see if the database already exists or if we have to create it.
 // Right now the system assumes it exists and dies if it doesn't
@@ -56,6 +58,14 @@ if (!mysql_select_db($database, $link)){
 //    echo "<br>\nSelected database $database succesfully<br>\n";
 }
 
+// Sub banner showing site stats. Size of database. Time.
+$rows_junk = mysql_query("SELECT SQL_CALC_FOUND_ROWS * FROM releases LIMIT 1");
+$rows_res = mysql_query("SELECT FOUND_ROWS()");
+$rows_total = mysql_fetch_row($rows_res);
+echo "<font style=\"font-size:smaller\"><b>$rows_total[0] releases in the database &middot ";
+echo date("g:m D\, M d\, Y");
+echo "</b></font>";
+
 // Main user input form
 echo <<<_END
 <p>Add to the database: <br>
@@ -64,13 +74,15 @@ echo <<<_END
 <label for="txtArtistName">Artist: </label> <input name="txtArtistName" class="input"/><br>
 <label for="txtReleaseName">Release: </label> <input name="txtReleaseName" class="input"/><br>
 <label for="txtLinkUrl">Link Url: </label> <input name="txtLinkUrl" class="input"/><br>
+<!-- Need to fix the location of this add button -->
 <br><input type="submit" class="button" value="Add" /></br>
 </form>
 _END;
 
 // A simple query -- Send a simple SQL query to the MySQL server and
 // print the results into an HTML table.
-$result = mysql_query('SELECT DISTINCT artist, release_name, time_added, link FROM releases ORDER BY time_added DESC');
+$query_limit = 20;
+$result = mysql_query("SELECT DISTINCT artist, release_name, time_added, link FROM releases ORDER BY time_added DESC LIMIT $query_limit");
 if(!$result){
     die('<p>Invalid query: ' . mysql_error());
 }
@@ -80,8 +92,8 @@ echo "<br>";
 // Build an HTML table from the CSS defined above
 echo "\n<table class=\"results\">";
 echo "\n<tr>";
-echo "<td align=\"left\"><b>Artist</b></td>\n";
-echo "<td alignt=\"left\"><b>Release</td></b>\n";
+echo "<td align=\"left\"><b>Artist - Release</b></td>\n";
+//echo "<td alignt=\"left\"><b>Release</td></b>\n";
 echo "<td align=\"center\"><b>Added</b></td>\n";
 echo "<td align=\"right\"><b>Link</b></td>\n";
 echo "</tr>\n";
@@ -94,13 +106,12 @@ while ($row = mysql_fetch_assoc($result)){
     } else {
         echo "\n<tr class=\"alt\">";
     } 
-    echo "<td align=\"left\">{$row['artist']}</td>\n";
-    echo "<td align=\"left\">{$row['release_name']}</td>";
+    print("<td align=\"left\">{$row['artist']} - {$row['release_name']}</td>\n");
+    //echo "<td align=\"left\">{$row['release_name']}</td>";
     $entry_age = findage($row['time_added']);
-    echo "<td align=\"center\">$entry_age</td>";
+    print("<td align=\"center\">$entry_age</td>");
     // Open the link in a new window or tab
-    echo "<td align=\"right\"><a href=\"{$row['link']}\" target=\"_blank\">{$row['link']}</a></td>";
-    echo "\n</tr>";
+    print("<td align=\"right\"><a href=\"{$row['link']}\" target=\"_blank\">{$row['link']}</a></td>\n</tr>\n");
     // Stop after a certain amount of results
     if ($table_idx == $table_length){
         break;
@@ -114,43 +125,50 @@ mysql_free_result($result);
 // Close the connection to the server
 mysql_close($link);
 
-// Convert the time diff_unix_time_now between now and the time a link was submitted into a
-// short, user-friendly statement 
+/*
+ * Convert the time diff_unix_time_now between now and the time a link was submitted into a
+ * short, user-friendly statement
+ */ 
 function findage($date)
 {
+    // The different type of units to be displayed.    
     $time_units = array("second", "minute", "hour", "day", "week", "month", "year");
     // Time divisors, rough estimates
-    $lengths = array("60","60","24","7","4.35","12","10");
-    // Rhe current time
+    $lengths = array("60",      // 60 seconds
+                     "60",      // 60 minutes
+                     "24",      // 24 hours
+                     "7",       // 7 days
+                     "4.35",    // ~4 weeks
+                     "12",      // 12 months
+                     "10");     // 10 years
+    // The current time
     $now = time();
     // Convert to unix format, # of secs since Jan 1, 1970
-    $unix_time  = strtotime($date);
+    $unix_time = strtotime($date);
     $diff_unix_time_now = $now - $unix_time;
     // Find the appropriate time unit to print
-    for($j = 0; $diff_unix_time_now >= $lengths[$j] && $j < count($lengths)-1; $j++) {
-        $diff_unix_time_now /= $lengths[$j];
+    for($units_idx = 0; $diff_unix_time_now >= $lengths[$units_idx] && $units_idx < count($lengths)-1; $units_idx++) {
+        $diff_unix_time_now /= $lengths[$units_idx];
     }
     $diff_unix_time_now = round($diff_unix_time_now);
     if($diff_unix_time_now != 1) {
-        $time_units[$j].= "s";
+        $time_units[$units_idx].= "s";
     }
     // Print a short statement in the table instead of the acutal time if the
     // entry has just been added
-    if(($diff_unix_time_now < 30) && ($j == 0)){
+    if(($diff_unix_time_now < 10) && ($units_idx == 0)){
         return "just now";
     } else {
-        return "$diff_unix_time_now $time_units[$j] ago";
+        return "$diff_unix_time_now {$time_units[$units_idx]} ago";
     }
 }
 
-
-
-
-?>
-
-<p><p>
+print <<<_END
 <p><p>&nbsp;<p>
 <div class = "footer">venu &copy; <a href="mailto:chrisbw@gmail.com">Chris Williams</a> 2012</div>
 <p>
 </body>
 </html>
+_END;
+
+?>
