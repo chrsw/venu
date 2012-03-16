73c3d790f1202bca4c7c0a69ab77e01c9ce79b2f
Chris Williams <chrisbw@gmail.com>
2012-03-16 12:30:13 -0400

    Removed db_login php script. Using plain text file for db auth instead.

--- db_login.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Use this for database login
-$test_database = "mysql";   // The database used for testing during development
-$database = "venu";         // The main music database
-$table = "releases";        // Name of table where actual release data is stored
-$host = "localhost";        // hostname of the mysql server
-$user = "admin";            // New MySQL user with privs for accessing the db
-$password = "password";     // DB access password, eventually there will be
-?>
